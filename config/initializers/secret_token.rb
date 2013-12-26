298b5499c91973dc037e619859defff1b76fd450
Yamitzky <negiga@gmail.com>
2013-12-26 23:44:53 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adventist::Application.config.secret_key_base = '9b2fc8bcc0303c21f6681c8a27210328b92f7700ede4a52d37902de5513c33e697bc85098bb0ecb9efa916dc0bd2d49308513628b3ad64857fae34a8e1edad08'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Adventist::Application.config.session_store :cookie_store, key: '_adventist_session'
